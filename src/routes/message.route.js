ca8834f1ddd3312c2845321b9d4e5043e4e68f53
janvi67 <117526245+janvi67@users.noreply.github.com>
2025-05-09 12:04:06 +0530

    Update message.route.js

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,12 +1,14 @@
-import express from "express";
+import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { deleteMessage, deleteAllMessages,getMessages, getUserForSidebar,sendMessage } from "../controllers/message.controller.js";
 
-const router = express.Router();
+const router=express.Router();
 
-router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
+router.get("/users",protectRoute,getUserForSidebar)
+router.get("/:id",protectRoute,getMessages)
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.delete("/delete/:id",protectRoute,deleteMessage);
+router.delete("/deleteAll/:id",protectRoute,deleteAllMessages);
 
-export default router;
+export default router
